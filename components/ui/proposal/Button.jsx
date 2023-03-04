d37ed412b8532203a5dd6444155758a922e8e634
Leonardo Citton <>
2023-03-04 10:56:10 +0100

    refactoring button icon

--- components/ui/proposal/Button.jsx
+++ components/ui/proposal/Button.jsx
@@ -21,7 +21,7 @@ export default function Button({ color = 'primary', size = 'base', text = '', hr
             <>
                 {size == 'base' && (
                     <svg
-                        width="auto" height="12px" viewBox="0 0 22 14" fill="none"
+                        width="20px" height="12px" viewBox="0 0 22 14" fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                     >
                         <path
@@ -33,7 +33,7 @@ export default function Button({ color = 'primary', size = 'base', text = '', hr
                 
                 {size == 'large' && (
                     <svg
-                        width="auto" height="16px" viewBox="0 0 22 14" fill="none"
+                        width="26px" height="16px" viewBox="0 0 22 14" fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                     >
                         <path
