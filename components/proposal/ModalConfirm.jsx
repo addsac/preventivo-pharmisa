d3cb86be15c932c555fd067400dd5a660394e342
Leonardo Citton <>
2023-03-04 10:44:10 +0100

    refactoring

--- components/proposal/ModalConfirm.jsx
+++ components/proposal/ModalConfirm.jsx
@@ -22,7 +22,7 @@ export default function ModalConfirm() {
             class="fixed inset-0 bg-gray-800/50 backdrop-blur-sm shadow-sm cursor-pointer"
         ></div>
         <div class="w-[calc(100%-48px)] h-auto max-w-[600px] rounded-xl shadow-xl bg-white absolute top-6 lg:top-16 left-1/2 transform translate-x-[-50%] p-6 lg:p-8 flex flex-col gap-y-6">
-            <div class="flex items-center justify-between">
+            <div class="flex items-start justify-between">
                 <p class="text-2xl font-medium"> Per confermare il preventivo: </p>
                 <button onClick={() => setConfirmModal(false)} className="bg-white w-10 h-10 border border-gray-300 hover:bg-gray-200 transition rounded-full text-gray-900 flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" class="w-4 h-4">
